98e6b5e2639f29075fb6b1ade79173dc55acdc35
shadowtool <meshalalhusainan@gmail.com>
2025-02-05 17:06:34 +0300

    centered header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,41 +1,31 @@
 import Link from "next/link";
 import AuthButton from "./AuthButton";
-import AddProperty from "../add-property/page";
 import AddPropertyButton from "./AddPropertyButton";
 
 const Header = () => {
-    return (  
+    return ( 
+        <section> 
         <header className="text-white p-6 shadow-md bg-gradient-to-r from-emerald-600 from-20% via-emerald-500 via-30% to-emerald-500 to-90%">
-          
+            <div className="container mx-auto flex justify-between items-center">
+                <img src="/images/beyt.png" alt="Logo" className="max-w-36 max-h-12" />
+
                 {/* Navigation Links */}
-                <nav className="flex-1 text-center">  
-                    <ul className="flex space-x-10 text-xl justify-center mx-auto">
+                <nav className="flex-0 flex justify-center items-center ml-72">
+                    <ul className="flex space-x-10 text-xl">
                         <li><Link href="/"><span className="hover:underline">Home</span></Link></li>
                         <li><Link href="/properties"><span className="hover:underline">Properties</span></Link></li>
                         <li><Link href="/contact"><span className="hover:underline">Contact</span></Link></li>
                     </ul>
                 </nav>
-                
-
-
-            <div className="container mx-auto max-h-24 flex justify-between items-center">
-            <img src="/images/beyt.png" alt="Logo" className="max-w-36 max-h-12 mr-12" />
-
-
 
                 {/* Auth Button */}
-
-                <div className="flex  justify-center items-center gap-2">
-                  
+                <div className="flex justify-center items-center gap-2">
                     <AuthButton />
                     <AddPropertyButton />
-                    
                 </div>
-
-               
-               
             </div>
         </header>
+        </section>
     );
 };
 
