6ec6d78cf83fdf06ceaf9a940b28b64727e9c8a6
shadowtool <meshalalhusainan@gmail.com>
2025-02-09 08:22:06 +0300

    Staging branch to collaborate latest version since talk on 2nd feb

--- app/components/FeaturedListings.jsx
+++ app/components/FeaturedListings.jsx
@@ -1,3 +1,5 @@
+
+
 // components/FeaturedListings.js
 import Image from 'next/image';
 
--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -5,18 +5,9 @@ import AddPropertyButton from "./AddPropertyButton";
 const Header = () => {
     return ( 
         <section> 
-        <header className="text-white p-6 shadow-md bg-gradient-to-r from-emerald-600 from-20% via-emerald-500 via-30% to-emerald-500 to-90%">
+        <header className="text-white p-0 shadow-md bg-gradient-to-r from-emerald-600 from-20% via-emerald-500 via-30% to-emerald-500 to-90%">
             <div className="container mx-auto flex justify-between items-center">
                 <img src="/images/beyt.png" alt="Logo" className="max-w-36 max-h-12" />
-
-                {/* Navigation Links */}
-                <nav className="flex-0 flex justify-center items-center ml-72">
-                    <ul className="flex space-x-10 text-xl">
-                        <li><Link href="/"><span className="hover:underline">Home</span></Link></li>
-                        <li><Link href="/properties"><span className="hover:underline">Properties</span></Link></li>
-                        <li><Link href="/contact"><span className="hover:underline">Contact</span></Link></li>
-                    </ul>
-                </nav>
 
                 {/* Auth Button */}
                 <div className="flex justify-center items-center gap-2">
@@ -25,6 +16,36 @@ const Header = () => {
                 </div>
             </div>
         </header>
+        <div className="text-white p-2 shadow-md bg-gradient-to-r from-emerald-600 from-20% via-emerald-500 via-30% to-emerald-500 to-90%">
+            <div className="container mx-auto flex justify-center items-center h-12">
+                {/* Navigation Links */}
+                <nav className="flex-1 flex justify-center items-center">
+                    <ul className="flex space-x-2 text-2xl">
+                        <li>
+                            <Link href="/">
+                                <span className="border-2 border-zinc-200 rounded-lg p-2 text-white transition-all duration-1000 hover:bg-emerald-400 hover:scale-105">
+                                    Home
+                                </span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/properties">
+                                <span className="border-2 border-zinc-200 rounded-lg p-2 text-white transition-all duration-1000 hover:bg-emerald-400 hover:scale-105">
+                                    Properties
+                                </span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/contact">
+                                <span className="border-2 border-zinc-200 rounded-lg p-2 text-white transition-all duration-1000 hover:bg-emerald-400 hover:scale-105">
+                                    Contact
+                                </span>
+                            </Link>
+                        </li>
+                    </ul>
+                 </nav>
+            </div>
+        </div>
         </section>
     );
 };
