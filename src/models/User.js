aea4ac5b46af4a7f8d640e298b4fcbce93e0d0c5
Mahesh Arya <manish0075arya@gmail.com>
2025-04-24 12:11:37 +0530

    user login fix

--- src/models/User.js
+++ src/models/User.js
@@ -5,7 +5,7 @@ const UserSchema = new mongoose.Schema(
   {
     name: String,
     email: { type: String, unique: true },
-    phoneNumber: { type: String, unique: true },
+    phoneNumber: { type: String, unique: true, sparse: true },
     password: { type: String },
     image: String,
     createdAt: { type: Date, default: Date.now },
