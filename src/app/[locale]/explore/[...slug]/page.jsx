c04611d2d0f1b50816b5c80cf6fcecd90bf90ebc
Mahesh Arya <manish0075arya@gmail.com>
2025-05-11 23:38:33 +0530

    build error fix

--- src/app/[locale]/explore/[...slug]/page.jsx
+++ src/app/[locale]/explore/[...slug]/page.jsx
@@ -1,25 +1,4 @@
 import ExploreProperties from "@/components/SearchedProperties/ExploreProperties";
-import { fetchPropertyListings } from "@/lib/queryFunctions";
-
-export async function generateStaticParams() {
-  const locales = ["en", "ar"];
-  const properties = await fetchPropertyListings(1, 10, {});
-
-  const params = [];
-
-  locales.forEach((locale) => {
-    properties.pages.forEach((page) => {
-      page.properties.forEach((property) => {
-        params.push({
-          locale,
-          slug: [property.status, property.type, property.location],
-        });
-      });
-    });
-  });
-
-  return params;
-}
 
 export default async function Page({ params }) {
   const { locale, slug } = params;
